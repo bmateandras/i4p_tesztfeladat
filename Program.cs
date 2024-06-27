12e0fd32ff13171343b20e2faa844e01ea41514b
bmateandras <bmateandras@gmail.com>
2024-06-27 17:55:16 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,266 +1,117 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Text;
-public class Kulcstalalo
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Tesztfeladat_1
 {
-    public  static string uzenet1;
-    public static string uzenet2;
-    public static string elsoval;
-    public static string kulcsreszlet;
-    public static string uzenetreszlet;
-    public static string kulcsreszlet2 = "";
-    public static string wordListPath = "words.txt";
-    public static string uzenetkieg = "";
-    public static string uzenet1reszletfejt = "";
-    public static string szotaras;
-    public static string feltet;
-    public static string kulcstovabb;
-    List<string> wordList = Fajltoltes(wordListPath);
-    public static List<string> karakterek = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " " };
-
-   
-    private static string kulcsreszletmegoldas(string uzenet1, string elsoval, List<string> karakterek)
+    public class Program
     {
-       
-        string kulcsreszlet = "";
-        int m = 0;
-        int kul = 0;
-        int masodik = 0;
-        for (int i = 0; i < elsoval.Length; i++)
+        public List<string> karakterek = new List<string>{ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " " };
+        public string uzenet = Console.ReadLine();
+        public string kulcs = Console.ReadLine();
+
+        public static string rejtjelezett(string uzenet, string kulcs, List<string> karakterek)
         {
-            int j = 0;
-            while (Convert.ToString(elsoval[i]) != karakterek[j])
-            {
-                j++;
-                
-            }
-            int elso = j;
+            string rejtjelezett = "";
+            int ossz = 0;
+            int masodik = 0;
+            int m = 0;
+            int i = 0;
 
-            for (int k = m; k < (m + 1); k++)
+            for (i = 0; i < uzenet.Length; i++)
             {
-                int l = 0;
-                while (Convert.ToString(uzenet1[k]) != karakterek[l])
+                int j = 0;
+                
+                while (Convert.ToString(uzenet[i]) != karakterek[j])
                 {
-                    l++;
+                    j++;
+                    if (karakterek.Contains(Convert.ToString(uzenet[i])) == false ) throw new ArgumentException("Próbálja az angol abc kiskarakteres betűivel");
                 }
-                
-                masodik = l;
+               
+                int elso = j;
 
+                for (int k = m; k < (m + 1); k++)
+                {
+                    int l = 0;
+                    while (Convert.ToString(kulcs[k]) != karakterek[l])
+                    {
+                        l++;
+                    }
+                    if (uzenet.Length > kulcs.Length) throw new ArgumentException("A kulcs legyen legalább olyan hosszú mint az üzenet.");
+                    masodik = l;
+
+                }
+                m++;
+                ossz = (elso + masodik);
+                rejtjelezett = rejtjelezett + karakterek[(ossz + 27) % 27];
+                
+               
             }
-            m++;
-            kul = (masodik - elso);
            
-            kulcsreszlet = kulcsreszlet + karakterek[(kul + 27) % 27];
+            return rejtjelezett;
+            
 
         }
-        return kulcsreszlet;
+        
 
-    }
-    private static string kulcsreszletmegoldas2(string uzenet2, string uzenetkieg, List<string> karakterek)
-    {
-       
-        int m = kulcsreszlet.Length;
-        int kul = 0;
-        int masodik = 0;
-        for (int i = 0; i < uzenetkieg.Length; i++)
+
+        public static string megoldas(string rejtjelezett, string kulcs, List<string> karakterek)
         {
-            int j = 0;
-            while (Convert.ToString(uzenetkieg[i]) != karakterek[j])
+            string megoldas = "";
+            int m = 0;
+            int kul = 0;
+            int masodik = 0;
+            for (int i = 0; i < rejtjelezett.Length; i++)
             {
-                j++;
-
-            }
-            int elso = j;
-
-            for (int k = m; k < (m + 1); k++)
-            {
-                int l = 0;
-                while (Convert.ToString(uzenet2[k]) != karakterek[l])
+                int j = 0;
+                while (Convert.ToString(rejtjelezett[i]) != karakterek[j])
                 {
-                    l++;
+                    j++;
+                    if (karakterek.Contains(Convert.ToString(rejtjelezett[i])) == false) throw new ArgumentException("Próbálja az angol abc kiskarakteres betűivel");
                 }
+                int elso = j;
 
-                masodik = l;
+                for (int k = m; k < (m + 1); k++)
+                {
+                    int l = 0;
+                    while (Convert.ToString(kulcs[k]) != karakterek[l])
+                    {
+                        l++;
+                    }
+                    if (rejtjelezett.Length > kulcs.Length) throw new ArgumentException("A kulcs legyen legalább olyan hosszú mint az üzenet.");
+                    masodik = l;
+
+                }
+                m++;
+                kul = (elso - masodik);
+                megoldas = megoldas + karakterek[(kul + 27) % 27];
+                
 
             }
-            m++;
-            kul = (masodik - elso);
+            return megoldas;
 
-            kulcsreszlet2 = kulcsreszlet2 + karakterek[(kul + 27) % 27];
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Adja meg a kódolandó üzenetet:");
+            string uzenet = Console.ReadLine();
+            Console.WriteLine("Adja meg a kódolandó üzenethez a kulcsot:");
+            string kulcs = Console.ReadLine();
+            List<string> karakterek = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " " };
+            Console.WriteLine("A rejtjelezett üzenet:");
+            Console.WriteLine(rejtjelezett(uzenet, kulcs, karakterek));
+            Console.WriteLine("Adja meg a megoldandó üzenetet:");
+            string kodolt = Console.ReadLine();
+            Console.WriteLine("Adja meg a megoldandó üzenet kulcsát:");
+            string kulcs2 = Console.ReadLine();
+            Console.WriteLine("A megoldott üzenet:");
+            Console.WriteLine(megoldas(kodolt, kulcs2, karakterek));
             
         }
-        return kulcsreszlet2;
 
     }
-    private static string uzenetreszletmegoldas(string uzenet1, string elsoval , string kulcsreszlet, List<string> karakterek)
-    {
-       
-        string uzenet2reszletmegoldas = "";
-        int m = 0;
-        int kul = 0;
-        int masodik = 0;
-        kulcsreszlet = kulcsreszletmegoldas(uzenet1, elsoval, karakterek);
-        for (int i = 0; i < kulcsreszlet.Length; i++)
-        {
-            int j = 0;
-            while (Convert.ToString(kulcsreszlet[i]) != karakterek[j])
-            {
-                j++;
-                if (karakterek.Contains(Convert.ToString(kulcsreszlet[i])) == false) throw new ArgumentException("Próbálja az angol abc kiskarakteres betűivel");
-            }
-            int elso = j;
-
-            for (int k = m; k < (m + 1); k++)
-            {
-                int l = 0;
-                while (Convert.ToString(uzenet2[k]) != karakterek[l])
-                {
-                    l++;
-                }
-
-                masodik = l;
-
-            }
-            m++;
-            kul = (masodik - elso);
-            uzenet2reszletmegoldas = uzenet2reszletmegoldas + karakterek[(kul + 27) % 27];
-
-        }
-        
-        return uzenet2reszletmegoldas;
-
-    }
-    private static string uzenetreszletmegoldas2(string uzenet1, string feltet, string kulcstovabb, List<string> karakterek)
-    {
-
-        string uzenetreszletmegoldas2 = "";
-        int m = 0;
-        int kul = 0;
-        int masodik = 0;
-        for (int i = 0; i < kulcstovabb.Length; i++)
-        {
-            int j = 0;
-            while (Convert.ToString(kulcstovabb[i]) != karakterek[j])
-            {
-                j++;
-                
-            }
-            int elso = j;
-
-            for (int k = m; k < (m + 1); k++)
-            {
-                int l = 0;
-                while (Convert.ToString(uzenet1[k]) != karakterek[l])
-                {
-                    l++;
-                }
-
-                masodik = l;
-
-            }
-            m++;
-            kul = (masodik - elso);
-            uzenetreszletmegoldas2 = uzenetreszletmegoldas2 + karakterek[(kul + 27) % 27];
-
-        }
-        
-        return uzenetreszletmegoldas2;
-        
-    }
-
-
-    private static List<string> Fajltoltes(string filePath)
-    {
-        return File.ReadAllLines(filePath).ToList();
-    }
- 
-    public static List<string> FindKey(string uzenet2reszletmegoldas, string uzenet2, string uzenet1, string elsoval, List<string> wordList)
-    {
-        
-        List<string> lehetkulcs = new List<string>();
-        List<string> karakterek = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " " };
-        
-            uzenet2reszletmegoldas = uzenetreszletmegoldas(uzenet1, elsoval, kulcsreszletmegoldas(uzenet1, elsoval, karakterek), karakterek);
-
-        if (uzenet2reszletmegoldas.Contains(" ") == false)
-        {
-            foreach (string word in wordList)
-            {
-                if (word.Contains(uzenet2reszletmegoldas))
-                {
-                    Console.WriteLine("Szótár szerint lehetséges:");
-                    Console.WriteLine(word);
-                }
-
-
-            }
-        }
-        else
-        {
-            Console.WriteLine("Adja meg az eddig sikeresen eltalált részt:");
-            string bekert = Console.ReadLine();
-            string szotaras = uzenet2reszletmegoldas.Substring((bekert.Length + 1), uzenet2reszletmegoldas.Length - (bekert.Length + 1));
-            foreach (string word in wordList)
-            {
-                if (word.StartsWith(szotaras))
-                {
-                    Console.WriteLine("Szótár szerint lehetséges:");
-                    Console.WriteLine(word);
-                }
-
-
-            }
-        }
-
-        Console.WriteLine("Adja meg a szóból a szótár alapján hiányzó karaktereket:");
-            uzenetkieg = Console.ReadLine();
-        kulcstovabb = kulcsreszlet + kulcsreszletmegoldas2(uzenet2, uzenetkieg, karakterek);
-         feltet = uzenet2reszletmegoldas + uzenetkieg;
-           
-            string kulcsresz = kulcstovabb;
-            uzenet1reszletfejt = uzenetreszletmegoldas2(uzenet1, feltet, kulcstovabb, karakterek);
-      
-        Console.WriteLine(uzenet1reszletfejt);
-        
-        lehetkulcs.Add(kulcsresz);
-        
-            return lehetkulcs;
-        
-    }
-    
-    public static void Main(string[] args)
-    {
-        
-        
-        Console.WriteLine("Adja meg a két kódolt üzenetet:");
-        uzenet1 = Console.ReadLine();
-        uzenet2 = Console.ReadLine();
-        List<string> karakterek = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " " };
-        string wordListPath = "words.txt";
-        List<string> wordList = Fajltoltes(wordListPath);
-        
-        Console.WriteLine("Adja meg a feltételezetten ismert részletet:");
-        elsoval = Console.ReadLine();
-        Console.WriteLine("Kulcsrészlet:");
-        kulcsreszlet = kulcsreszletmegoldas(uzenet1, elsoval, karakterek);
-        Console.WriteLine(kulcsreszlet);
-        uzenetreszlet = uzenetreszletmegoldas(uzenet1, elsoval, kulcsreszlet, karakterek);
-        Console.WriteLine("Üzenetrészlet:");
-        Console.WriteLine(uzenetreszlet);
-        List<string> lehetkulcs = FindKey(uzenetreszlet, uzenet2, uzenet1, elsoval, wordList);
-       
-
-        foreach (string kulcsresz in lehetkulcs)
-        {
-          Console.WriteLine("Lehetséges kulcs: " + kulcsresz);
-        }
-       
-         Console.WriteLine("Amennyiben nincs meg valamelyik üzenet teljes kódja futassa újra a programot a szerzett információval.");
-
-
-    }
-
-
 }
+    
+
